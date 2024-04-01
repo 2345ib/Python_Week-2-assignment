3d232829d12df8dd3e44f83f8da267d8d8c2f47e
2345ib <159881377+2345ib@users.noreply.github.com>
2024-04-01 06:44:15 -0700

    Add files via upload

---
+++ Assignments.py
@@ -0,0 +1,19 @@
+my_list = []
+print(my_list)
+#making an empty list
+
+my_list.append([10,20,30,40])
+print(my_list)
+#appending an empty list
+
+my_list.extend([50,60,70])
+print(my_list)
+#extending my list
+
+my_list.remove(70)
+print(my_list)
+#removing elements to my list
+
+my_list = [20,10,30,50,40,60]
+my_list.sort()
+print(my_list)
